44dfdc9831cf27ebeb225662efbf530081d261e2
jdaniel01 <professor4cob@outlook.com>
2021-05-20 09:09:46 -0700

    phase 3

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const csrf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const {ValidationError} = require('sequelize');
 
 const {environment} = require('./config');
 const isProduction = environment === 'production';
@@ -29,6 +30,33 @@ app.use(csrf({
 }));
 
 app.use(indexRouter);
+
+
+app.use((req, res, next) => {
+    const err = new Error('Wipe out. The resource you were looking for could not be found.');
+    err.title = "Resource Not Found";
+    err.errors = ["Wipe out. The resource you were looking for could not be found."];
+    err.status = 404;
+    next(err);
+});
+app.use((err, req, res, next) => {
+    if(err instanceof ValidationError) {
+        err.errors = err. errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+})
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    })
+});
+
 
 
 module.exports = app;
