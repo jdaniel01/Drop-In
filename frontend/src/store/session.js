69384ed4c4debdd480a2983f36274f448792dcbb
jdaniel01 <professor4cob@outlook.com>
2021-05-21 14:30:57 -0700

    added logout thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -60,6 +60,14 @@ export const signup = (user) => async (dispatch) => {
     return response;
 };
 
+export const logoutUser = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
+
 const sessionReducer = (state = initialState, action) => {
     let newState;
     switch (action.type) {
