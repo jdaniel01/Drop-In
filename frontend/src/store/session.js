2baff79e5a11ed7f235995571c3a93d3c52f134c
jdaniel01 <professor4cob@outlook.com>
2021-05-21 13:44:49 -0700

    finished login Redux actions, thunk and reducers

---
+++ frontend/src/store/session.js
@@ -0,0 +1,58 @@
+import { csrfFetch } from './csrf';
+
+const initialState = { user: null };
+
+const SET_USER = 'session/SET_USER';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user
+    };
+};
+
+const removeUser = (user) => {
+    return {
+        type: REMOVE_USER
+    };
+};
+
+
+export const loginUser = (user) => async (dispatch) => {
+    const {credential, password} = user;
+    const res = await csrfFetch('/api/session', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            credential,
+            password
+        })
+    })
+    if (res.ok) {
+        const sessionUser = await res.json();
+        dispatch(setUser(sessionUser.user))
+        return res;
+    }
+}
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+
+export default sessionReducer;
