d23207e9c76acf76c89a9833b30b063477ddfd90
jdaniel01 <professor4cob@outlook.com>
2021-05-21 09:38:17 -0700

    set up CSRF protection

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,12 +4,39 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    router.get('/', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+
+    router.use(express.static(path.resolve("../frontend/build")));
+
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.json({});
+    });
+}
+
+
 //test route below:
 // router.get('/hello/world', (req, res)=>{
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
 //     res.send('Droppin!')
 // })
-
+//test route above:
 
 
 
