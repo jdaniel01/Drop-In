7b3d33b7ee233f1fc5dee3d36c65bb6eb5147b5f
jdaniel01 <professor4cob@outlook.com>
2021-05-25 10:49:25 -0700

    updated routes/api/location.js

--- backend/routes/api/locations.js
+++ backend/routes/api/locations.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const {check} = require('express-validator');
-const {User, Location, State, Country, City} = require('../../db/models');
+const {Location} = require('../../db/models');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
@@ -20,22 +20,47 @@ const validateLocation = [
     check('description')
         .exists({checkFalsy: true})
         .withMessage("Please provide a description of the location."),
+    check('city_id')
+        .exists({checkFalsey: true})
+        .withMessage('Please choose a city for this location.'),
+    check('state_id')
+        .exists({checkFalsey: true})
+        .withMessage('Please choose a state for this location.'),
+    check('country_id')
+        .exists({checkFalsey: true})
+        .withMessage('Please choose a country for this location.'),
+    check('image')
+        .exists({checkFalsey: true})
+        .withMessage('Please provider an image for this location.')
+        .isString()
+        .withMessage('Image must be provided in url format.'),
+    check('website')
+        .exists({checkFalsey: true})
+        .withMessage('Please provide a website for this location.'),
+    check('address')
+        .exists({checkFalsey: true})
+        .withMessage('Please provide an address for this location.'),
     handleValidationErrors
 ]
 
 router.get('/', asyncHandler(async (req, res) => {
     const locations = await Location.findAll();
     return res.json(locations);
-}))
+}));
 
-router.post('/', validateLocation, asyncHandler(async (req, res, next)=> {
+
+router.post('/', validateLocation, asyncHandler(async (req, res)=> {
     const {name, city_id, state_id, country_id, description, image, website, address} = req.body;
-    if (name && city_id && state_id && country_id && description && image && website && address){
+
         const location = {name, city_id, state_id, country_id, description, image, website, address};
         let data = await Location.create(location);
         res.redirect(`/${data.id}`);
-    } else {let err =  Error("There was an error. Please try again."); next(err)}
 }));
 
+router.get('/:id', asyncHandler(async (req, res) => {
+    const {id} = req.params;
+    const location = await Location.findByPk(id);
+    return res.json(location);
+}));
 
 module.exports = router;
