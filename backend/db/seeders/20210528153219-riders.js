f87cb5821a6c92fa1d890c6ca450b21c299682d5
jdaniel01 <professor4cob@outlook.com>
2021-05-28 08:44:33 -0700

    added seeder for tricks

---
+++ backend/db/seeders/20210528153219-riders.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Riders', [
+     {user_id: 1, event_id: 1, createdAt: new Date(), updatedAt: new Date()}
+   ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Riders', null, {});
+  }
+};
